40fc4a29ed30774fc3a34cd8f3d34043042511a9
ZIMMERMANEJ22 <ZimmermanEJ22@gcc.edu>
2025-02-17 15:38:42 -0500

    nice try

--- src/gitStuff.java
+++ src/gitStuff.java
@@ -2,6 +2,6 @@ public class gitStuff {
     public static void main(String[] args) {
         System.out.println("Heyo");
         System.out.println("hey");
-        System.out.println("A newer line");
+        System.out.println("A newish line");
     }
 }
