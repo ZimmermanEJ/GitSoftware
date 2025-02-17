1b1a7ce51bf86bedd0cc1b15f95779f66f5ff891
ZIMMERMANEJ22 <ZimmermanEJ22@gcc.edu>
2025-02-17 15:25:17 -0500

    Added a new line

--- src/gitStuff.java
+++ src/gitStuff.java
@@ -2,5 +2,6 @@ public class gitStuff {
     public static void main(String[] args) {
         System.out.println("Heyo");
         System.out.println("hey");
+        System.out.println("A new line");
     }
 }
